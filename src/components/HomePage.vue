35f42fb5e09ae1f5f2d65a4e7066fd1a95e13af4
443035629 <443035629@qq.com>
2024-08-06 17:26:06 +0800

    123

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,10 +1,8 @@
 <script setup>
-import VUE from 'vue'
+// import VUE from 'vue'
 </script>
 
 <template>
   <div>statt 123121113 </div>
-  <div>
-    <a href="https://xystarxy.github.io/Jupyterlite">Jupyterlite</a>
-  </div>
+  <div>    <a href="https://xystarxy.github.io/Jupyterlite">Jupyterlite</a>  </div>
 </template>
