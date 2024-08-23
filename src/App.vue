8ac1c0a1d38d2fdfa1c1e5cee45d6764e68c06d0
443035629 <443035629@qq.com>
2024-08-23 14:10:30 +0800

    页面调整

--- src/App.vue
+++ src/App.vue
@@ -5,13 +5,11 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="https://xystarxy.github.io/Jupyterlite">Jupyterlite</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
@@ -26,10 +24,6 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
 nav {
   width: 100%;
